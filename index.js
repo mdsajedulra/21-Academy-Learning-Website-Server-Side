23c1287ef84f602b0ac22fa5788054d90f5693bc
mdsajedulra <mdsajedulra@gmail.com>
2022-10-27 09:44:32 +0600

    express setup

--- index.js
+++ index.js
@@ -9,26 +9,7 @@ const categories = require('./data/Categories.json')
 const checkout = require('./data/checkoutAPI.json')
 
 
-app.get('/', (req, res) => {
-    res.send('Api is running')
-})
-app.get('/courses', (req, res) => {
-    res.send(courses)
-})
-app.get('/categories', (req, res) => {
-    res.send(categories)
-})
-app.get('/checkout/:id', (req, res) => {
-    const id = req.params.id;
-    const selectedCheckout = checkout.find(item => item.category_id == id);
-    res.send(selectedCheckout)
-})
 
-app.get('/courses/:id', (req, res) => {
-    const id = req.params.id;
-    const selectedItem = courses.find(item => item.category_id == id);
-    res.send(selectedItem)
-})
 app.listen(port, () => {
     console.log(`example ${port}`)
 })

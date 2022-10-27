2910e2e28f3f9b146699f93df17a6d5046974eca
mdsajedulra <mdsajedulra@gmail.com>
2022-10-27 09:51:46 +0600

    some dynamic api create

--- index.js
+++ index.js
@@ -18,7 +18,17 @@ app.get('/courses', (req, res) => {
 app.get('/categories', (req, res) => {
     res.send(categories)
 })
+app.get('/checkout/:id', (req, res) => {
+    const id = req.params.id;
+    const selectedCheckout = checkout.find(item => item.category_id == id);
+    res.send(selectedCheckout)
+})
 
+app.get('/courses/:id', (req, res) => {
+    const id = req.params.id;
+    const selectedItem = courses.find(item => item.category_id == id);
+    res.send(selectedItem)
+})
 app.listen(port, () => {
     console.log(`example ${port}`)
 })
